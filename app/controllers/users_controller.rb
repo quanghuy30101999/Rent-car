fac2d661c4dc6eec53fa247c5e2d92ff468a4152
quanghuy <huytran.301099@gmail.com>
2020-03-01 08:47:51 +0700

    done validate-email

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,13 +11,14 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in @user
-      flash[:success] = "Welcome to the Rent Car"
-      redirect_to @user
+      @user.send_activation_email
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
     else
       render 'new'
     end
   end
+
 
   def edit
     @user = User.find(params[:id])
