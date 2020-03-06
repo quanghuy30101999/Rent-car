f5ac2edbf416c4440bf972980e9ed2ff62324b7a
quanghuy <huytran.301099@gmail.com>
2020-03-06 09:11:06 +0700

    them I18n

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,8 +8,13 @@ class ApplicationController < ActionController::Base
     def logged_in_user
       unless logged_in?
         store_location
-        flash[:danger] = "Please log in."
+        flash[:danger] = I18n.t(:login_messeage)
         redirect_to login_url
       end
+    end
+
+    def switch_locale(&action)
+      locale = params[:locale] || I18n.default_locale
+      I18n.with_locale(locale, &action)
     end
 end
