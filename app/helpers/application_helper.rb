b10e6a927e6b722f5f7fda1f9d3d7014d5a81ae3
quanghuy <huytran.301099@gmail.com>
2020-03-15 09:09:36 +0700

    refactor

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,22 @@
 module ApplicationHelper
 
   # Returns the full title on a per-page basis.
-  def full_title(page_title = '')
+  def full_title(page_title = "")
     base_title = "Rent Car"
     if page_title.empty?
       base_title
     else
       page_title + " | " + base_title
+    end
+  end
+
+  def format_currency(price, price_unit)
+    price_units = "đ"
+    if price_unit == "USD"
+      price_units = "$"
+      format_currency = price.to_s + price_units
+    else
+      format_currency = price.to_s + price_units
     end
   end
 end
--- app/helpers/cars_helper.rb
+++ app/helpers/cars_helper.rb
@@ -1,10 +1,2 @@
 module CarsHelper
-  def price_unit(car)
-    price_unit = "đ"
-    if car.price_unit == "USD"
-      price_unit = "$"
-    else
-      price_unit
-    end
-  end
 end
