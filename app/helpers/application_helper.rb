8f48177da414f23396d88611d62bbd0267f82838
quanghuy <huytran.301099@gmail.com>
2020-03-25 16:49:28 +0700

    update application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,18 +11,13 @@ module ApplicationHelper
   end
 
   def format_currency(price, price_unit)
-    price_symbol = "đ"
-    if price_unit == "USD"
-      price_symbol = "$"
-      "#{price_symbol}#{price}"
-    else
-      "#{price}#{price_symbol}"
-    end
+    price_symbol = (price_unit == "USD") ? "$" : "đ"
+    (price_unit == "USD") ? "#{price_symbol}#{price}" : "#{price}#{price_symbol}"
   end
 
   def user_oreded?(car_id)
     current_user.orders.any? do |order|
-      order.car_id == car_id && order.orders_status == "requesting"
+      order.car_id == car_id && order.requesting?
     end
   end
 end
